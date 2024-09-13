f0d9a2b9933cb13d949286ba120fe25e72cf2eee
danie <danie@DESKTOP-UA8MPDR.mynetworksettings.com>
2024-09-12 20:23:28 -0400

    Second change

--- src/team2/Team2Demo.java
+++ src/team2/Team2Demo.java
@@ -4,6 +4,8 @@ public class Team2Demo {
 	public static void main(String[] args) {
 		System.out.println("Day 2 demo...");
 		System.out.println("group 2 is the best");
+		
+		System.out.println("Great group");
 	}
 
 }
