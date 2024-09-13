c13fe590c6029747b7b31d8e62f699c5e30c86f5
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-12 20:11:37 -0400

    Demo Push

--- src/team4/Team4Demo.java
+++ src/team4/Team4Demo.java
@@ -4,5 +4,10 @@ public class Team4Demo {
 	public static void main(String[] args) {
 		System.out.println("Day 2 demo...");
 	}
+	
+	public static void show() {
+		System.out.println("The team 4 is very good");
+		System.out.println("We will work in importants companies");
+	}
 
 }
