618a05be98cec3426825938249f901bc5adfd0b4
Ahmed <Ahmed@DESKTOP-I1FBRI5.lan>
2024-09-12 20:24:00 -0400

    new Method added

--- src/team4/Team4Demo.java
+++ src/team4/Team4Demo.java
@@ -4,13 +4,14 @@ public class Team4Demo {
 	public static void main(String[] args) {
 		System.out.println("Day 2 demo...");
 	}
-	
+
 	public static void show() {
 		System.out.println("The team 4 is very good");
 		System.out.println("We will work in importants companies");
 	}
+
 	public static void team4() {
 		System.out.println("The team 4 is very good");
 		System.out.println("We will work in importants companies");
-}
+	}
 }
