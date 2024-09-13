5bc103a1cba01e9b1fc8f6db2157d24c987e5ca2
Merri839 <Merhhawitandit@gmail.com>
2024-09-12 17:33:09 -0700

    Update Team1Demo.java

--- src/team1/Team1Demo.java
+++ src/team1/Team1Demo.java
@@ -4,13 +4,10 @@ public class Team1Demo {
 	public static void main(String[] args) {
 		System.out.println("Day 2 demo...");
 	}
-public static void Team1() {
-	System.out.println("Test 1");
+public static void test1() {
+	System.out.println("Merri");
 	}
 	
-	public static void Team1() {
-		
-		System.out.println("Test 1");
-	}
+	
 
 }
